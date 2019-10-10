003aa072fb097f9d68fb9836b46253f98bc6f0e5
nmhieu1896 <nmhieu1896@gmail.com>
2019-10-10 17:57:42 +0700

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,6 @@ function App() {
   return (
     <div className="App">
       <h3>Hello</h3>
-<<<<<<< HEAD
-=======
-
-      <p>Hôm nay là thứ 5</p>
-      <p>HÔm nay trời mưa</p>
-
-      <div>
-        <p>Bây giờ là buổi chiều</p>
-       
-      </div>
-      <h2>hello</h2>
->>>>>>> 090023284a3a7c1912c039c1501594ebcb62c07a
     </div>
   );
 }
